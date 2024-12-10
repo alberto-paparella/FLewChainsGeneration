c8e8f3084a176a8033456af1efa02839bad6562a
Alberto Paparella <pprlrt@unife.it>
2024-12-11 01:41:59 +1000

    Added associativity check and minor error checks

--- core.jl
+++ core.jl
@@ -24,7 +24,14 @@ struct FLewMonoid{N}
     o::Vector{Int}
     
     function FLewMonoid{N}(o::Vector{Int}) where {N}
+        length(o) != ((N-2)*(N-1))÷2 && error("Wrong number of elements provided")
         new{N}(o)
+    end
+
+    function FLewMonoid{N}() where {N}
+        N < 1 && error("N must be greater than 0")
+        (N == 1 || N == 2) && return FLewMonoid{N}(Vector{Int}())
+        N > 2 && error("Please provide elements for N > 2")
     end
 end
 
@@ -42,6 +49,7 @@ function Base.show(io::IO, fm::FLewMonoid{N}) where {N}
             ct[i,j] = ct[j,i] = evaluate(fm, i-1, j-1)
         end
     end
+    print(io, "\n")
     print(io, " ⋅ |")
     for i in 1:N print(io, " $(Int(i-1))") end
     print(io, "\n")
@@ -57,11 +65,11 @@ end
 function evaluate(fm::FLewMonoid{N}, a::Int, b::Int) where {N}
     a == 0 && return 0
     b == 0 && return 0
-    a == N-1 && return a
-    b == N-1 && return b
+    a == N-1 && return b
+    b == N-1 && return a
     a > N-1 || a < 0 && error("a is not in the domain")
     b > N-1 || b < 0 && error("b is not in the domain")
-    fm.o[(N-2)*(a-1)+b-(a*(a-1))÷2]
+    a < b ? fm.o[(N-2)*(a-1)+b-(a*(a-1))÷2] : fm.o[(N-2)*(b-1)+a-(b*(b-1))÷2]
 end
 
 function weaklyincreasing(q, seq::Vector{Int}, min::Int, n::Int, l::Int)
@@ -86,29 +94,55 @@ end
 weaklyincreasing(min::Int, n::Int,l::Int) = weaklyincreasing([], Vector{Int}(), min, n, l)
 weaklyincreasing(n::Int,l::Int) = weaklyincreasing([], Vector{Int}(), 0, n, l)
 
-function generateflewchain(q, o, min, n, l)
+function generateflewchain(q::Vector{Vector{Int}}, o::Vector{Int}, min::Int, n::Int, l::Int)
     if l == 1
-        wi = weaklyincreasing(last(o), n, l)
+        wi = weaklyincreasing(min, n, l)
         for i in wi
             push!(q, vcat(o, i))
         end
         return q    
     else
-            wi = weaklyincreasing(min, n, l)
-            for i in wi
-                iswi = true
-                if l != n-2
-                    for j in 1:length(i)
-                        if reverse(i)[j] < reverse(o)[j]
-                            iswi = false
-                            break
-                        end
+        wi = weaklyincreasing(min, n, l)
+        for i in wi
+            iswi = true
+            if l != n-2
+                for j in 1:length(i)
+                    if reverse(i)[j] < reverse(o)[j]
+                        iswi = false
+                        break
                     end
                 end
-                iswi && generateflewchain(q, vcat(o, i), i[1], n, l-1)
             end
-            return q
+            iswi && generateflewchain(q, vcat(o, i), i[2], n, l-1)
         end
+        return q
+    end
 end
 
-generateflewchain(n, l) = generateflewchain([], Vector{Int}(), 0, n, l)
+function generateflewchain(n)
+    n < 3 && return Vector{Vector{Int}}()
+    generateflewchain(Vector{Vector{Int}}(), Vector{Int}(), 0, n, n-2)
+end
+
+function evaluate(o::Vector{Int}, a::Int, b::Int, n::Int)
+    a == 0 && return 0
+    b == 0 && return 0
+    a == n-1 && return b
+    b == n-1 && return a
+    a < b ? o[(n-2)*(a-1)+b-(a*(a-1))÷2] : o[(n-2)*(b-1)+a-(b*(b-1))÷2]
+end
+
+function checkassociativity(o::Vector{Int}, n::Int)
+    for α in 1:n-2
+        Δ = Vector{Int}()
+        ∇ = Vector{Int}()
+        for x in 1:n-2
+            for y in 1:n-2
+                push!(Δ, evaluate(o, evaluate(o, x, α, n), y, n))
+                push!(∇, evaluate(o, x, evaluate(o, α, y, n), n))
+            end
+        end
+        Δ != ∇ && return false
+    end
+    return true
+end
