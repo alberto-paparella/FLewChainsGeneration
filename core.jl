e84888810fc3b283505aff3eccab1c419266691b
Alberto Paparella <pprlrt@unife.it>
2024-12-11 17:16:23 +1000

    Added some optimizations and multi-threading

--- core.jl
+++ core.jl
@@ -1,15 +1,22 @@
 """
-Given a set A of N linearly ordered elements, A = {0, ..., N-1}, and
+    struct FiniteFLewChain{N}
+        cayleytable::Vector{Int8}
+    end
+
+A structure representing a finite FLew-chain over a set of N linearly ordered elements.
+
+Given a set A of N linearly ordered elements A = {0, ..., N-1}, 
 C_N = {0, 1/(N-1), 2/(N-1), ..., (N-2)/(N-1), (N-1)/(N-1) = 1}.
 
-We could represent the magma (C_N) with an N × N matrix, using N^2 bytes.
- - Since the magma is abelian, we just need to represent its upper triangular
-   using (N*(N+1))÷2 bytes.
- - Since 1 is an identity element and 0 an absorbing element, we don't need
+We could represent the Cayley's table for (C_N, ⋅) with an N × N matrix, but:
+ - since the magma is abelian, we just need to represent its upper triangular
+   using an (N*(N+1))÷2 vector;
+ - since 1 is an identity element and 0 an absorbing element, we don't need
    to represent the first and last rows/columns, so we'd just need a (N-2)^2 matrix.
 Taking both points into consideration, we just need an ((N-2)(N-1))÷2 vector.
 
 E.g., with N=5:
+
    ⋅  |  0  1/4 2/4 3/4  1
  ===========================
    0  |  0   0   0   0   0
@@ -20,24 +27,30 @@ E.g., with N=5:
 
 We just need {A, B, C, D, E, F} (6 elements instead of 25!!)
 """
-struct FLewMonoid{N}
-    o::Vector{Int}
+struct FiniteFLewChain{N}
+    cayleytable::Vector{Int8}
     
-    function FLewMonoid{N}(o::Vector{Int}) where {N}
-        length(o) != ((N-2)*(N-1))÷2 && error("Wrong number of elements provided")
-        new{N}(o)
+    function FiniteFLewChain{N}(cayleytable::Vector{Int8}) where {N}
+        if length(cayleytable) != ((N-2)*(N-1))÷2
+            error("Wrong number of elements provided")
+        end
+        return new{N}(cayleytable)
     end
 
-    function FLewMonoid{N}() where {N}
-        N < 1 && error("N must be greater than 0")
-        (N == 1 || N == 2) && return FLewMonoid{N}(Vector{Int}())
-        N > 2 && error("Please provide elements for N > 2")
+    function FiniteFLewChain{N}() where {N}
+        if N < 1
+            error("N must be greater than 0")
+        elseif N == 1 || N == 2
+            return FiniteFLewChain{N}(Vector{Int8}())
+        else
+            return N > 2 && error("Please provide elements for N > 2")
+        end
     end
 end
 
 import Base.show
 
-function Base.show(io::IO, fm::FLewMonoid{N}) where {N}
+function Base.show(io::IO, fm::FiniteFLewChain{N}) where {N}
     ct = Matrix{Union{Nothing, Int}}(nothing, N, N)
     for i in 1:N
         ct[1,i] = ct[i,1] = 0
@@ -45,7 +58,6 @@ function Base.show(io::IO, fm::FLewMonoid{N}) where {N}
     end
     for i in 2:N-1
         for j in i:N-1
-            # ct[i,j] = ct[j,i] = fm.o[(N-2)*(i-2)+j-1-((i-1)*(i-2))÷2]
             ct[i,j] = ct[j,i] = evaluate(fm, i-1, j-1)
         end
     end
@@ -62,17 +74,17 @@ function Base.show(io::IO, fm::FLewMonoid{N}) where {N}
     end
 end
 
-function evaluate(fm::FLewMonoid{N}, a::Int, b::Int) where {N}
+function evaluate(fm::FiniteFLewChain{N}, a::Int, b::Int) where {N}
     a == 0 && return 0
     b == 0 && return 0
     a == N-1 && return b
     b == N-1 && return a
-    a > N-1 || a < 0 && error("a is not in the domain")
-    b > N-1 || b < 0 && error("b is not in the domain")
-    a < b ? fm.o[(N-2)*(a-1)+b-(a*(a-1))÷2] : fm.o[(N-2)*(b-1)+a-(b*(b-1))÷2]
+    (a > N-1 || a < 0) && error("a is not in the domain")
+    (b > N-1 || b < 0) && error("b is not in the domain")
+    a < b ? fm.cayleytable[(N-2)*(a-1)+b-(a*(a-1))÷2] : fm.cayleytable[(N-2)*(b-1)+a-(b*(b-1))÷2]
 end
 
-function weaklyincreasing(q, seq::Vector{Int}, min::Int, max::Int, l::Int)
+function weaklyincreasing(q, seq::Vector{Int8}, min::Int, max::Int, l::Int)
     if l == 0
         push!(q, copy(seq))
     else
@@ -86,17 +98,21 @@ function weaklyincreasing(q, seq::Vector{Int}, min::Int, max::Int, l::Int)
 end
 
 
-weaklyincreasing(min::Int, max::Int, l::Int) = weaklyincreasing(Vector{Vector{Int}}(), Vector{Int}(), min, max, l)
+weaklyincreasing(min::Int, max::Int, l::Int) = weaklyincreasing(Vector{Vector{Int8}}(), Vector{Int8}(), min, max, l)
 
-function generateflewchain(q::Vector{Vector{Int}}, o::Vector{Int}, min::Int, max::Int, l::Int)
+function generateflewchain(q::Vector{Vector{Int8}}, o::Vector{Int8}, min::Int, max::Int, l::Int, n::Int, u)
     wi = weaklyincreasing(min, max, l) 
     if l == 1
         for i in wi
-            push!(q, vcat(o, i))
+            if checkassociativity(vcat(o, i), n)
+                Threads.lock(u) do
+                    push!(q, vcat(o, i))
+                end
+            end
         end
     else
         wi = weaklyincreasing(min, max, l)
-        for i in wi
+        Threads.@threads for i in wi
             iswi = true
             if !isempty(o)
                 for j in 1:length(i)
@@ -106,19 +122,19 @@ function generateflewchain(q::Vector{Vector{Int}}, o::Vector{Int}, min::Int, max
                     end
                 end
             end
-            iswi && generateflewchain(q, vcat(o, i), i[2], max+1, l-1)
+            iswi && generateflewchain(q, vcat(o, i), Int64(i[2]), max+1, l-1, n, u)
         end
     end
 end
 
 function generateflewchain(n)
-    q = Vector{Vector{Int}}()
+    q = Vector{Vector{Int8}}()
     n < 3 && return q
-    generateflewchain(q, Vector{Int}(), 0, 1, n-2)
+    generateflewchain(q, Vector{Int8}(), 0, 1, n-2, n, Threads.SpinLock())
     return q
 end
 
-function evaluate(o::Vector{Int}, a::Int, b::Int, n::Int)
+function evaluate(o::Vector{Int8}, a::Int, b::Int, n::Int)
     a == 0 && return 0
     b == 0 && return 0
     a == n-1 && return b
@@ -126,25 +142,22 @@ function evaluate(o::Vector{Int}, a::Int, b::Int, n::Int)
     a < b ? o[(n-2)*(a-1)+b-(a*(a-1))÷2] : o[(n-2)*(b-1)+a-(b*(b-1))÷2]
 end
 
-function checkassociativity(o::Vector{Int}, n::Int)
-    for α in 1:n-2
-        Δ = Vector{Int}()
-        ∇ = Vector{Int}()
-        for x in 1:n-2
-            for y in 1:n-2
-                push!(Δ, evaluate(o, evaluate(o, x, α, n), y, n))
-                push!(∇, evaluate(o, x, evaluate(o, α, y, n), n))
+function checkassociativity(o::Vector{Int8}, n::Int)
+    for a in 1:n-2
+        for b in 1:n-2
+            for c in 1:n-2
+                evaluate(o, Int64(evaluate(o, a, b, n)), c, n) != evaluate(o, a, Int64(evaluate(o, b, c, n)), n) && return false
             end
         end
-        Δ != ∇ && return false
     end
     return true
 end
 
-function generateflewchains(n)
+function generateflewchains(n::Int)
     if n < 3
-        FLewMonoid{n}()
+        FiniteFLewChain{n}()
     else
-        FLewMonoid{n}.([filter(x -> checkassociativity(x, n)==1, generateflewchain(n))...])
+        # FiniteFLewChain{n}.([filter(x -> checkassociativity(x, n)==1, generateflewchain(n))...])
+        FiniteFLewChain{n}.([generateflewchain(n)...])
     end
 end
